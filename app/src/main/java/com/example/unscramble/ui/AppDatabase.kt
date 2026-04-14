430f4490fd1cced9978ab010b695f59563ff64fc
Ghofur Akbar <infamousg035@gmail.com>
2026-04-14 15:10:09 +0700

    menambahkan Room ke database

---
+++ app/src/main/java/com/example/unscramble/ui/AppDatabase.kt
@@ -0,0 +1,9 @@
+package com.example.unscramble.ui
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [GameUiState::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun HistoryDao(): GameUiStateDAO
+}
