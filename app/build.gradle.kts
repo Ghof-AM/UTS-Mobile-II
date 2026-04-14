0d94cda3e154e1a590b4a63d4a7e4b4567ae700d
Ghofur Akbar <infamousg035@gmail.com>
2026-04-14 22:58:27 +0700

    penambahan/pengaktifan plugin ksp di level root untuk solve ksp error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,16 +18,17 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
+    id("com.google.devtools.ksp")
 }
 
 android {
     namespace = "com.example.unscramble"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.unscramble"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -65,12 +66,13 @@ android {
 
 dependencies {
 
-    val room_version = "2.7.0"
+    val room_version = "2.6.0"
 
     implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
     ksp("androidx.room:room-compiler:$room_version")
 
-    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
 
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.activity:activity-compose:1.9.3")
